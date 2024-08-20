7806db777123b8519b429a84c12a9d191c1b6daa
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-08-20 14:06:41 +0600

    Added Daisy UI setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
